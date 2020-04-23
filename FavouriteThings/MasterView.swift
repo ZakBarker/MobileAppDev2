03c449eaa3ef5c5e7f97af6142eb6639cfcc096e
Zak Barker <zakobama@hotmail.com>
2020-04-23 13:49:03 +1000

    First Commit

---
+++ FavouriteThings/MasterView.swift
@@ -0,0 +1,21 @@
+//
+//  MasterView.swift
+//  FavouriteThings
+//
+//  Created by Zak Barker on 23/4/20.
+//  Copyright © 2020 Zak Barker. All rights reserved.
+//
+
+import SwiftUI
+
+struct MasterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MasterView_Previews: PreviewProvider {
+    static var previews: some View {
+        MasterView()
+    }
+}
