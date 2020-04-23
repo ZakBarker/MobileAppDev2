ab19aeaba6c2ec0e4e16c2d48ccbba8bdfb4c30a
Zak Barker <zakobama@hotmail.com>
2020-04-23 14:25:02 +1000

    Updated MasterView with functionality - Yet to integrate

--- FavouriteThings/MasterView.swift
+++ FavouriteThings/MasterView.swift
@@ -8,14 +8,22 @@
 
 import SwiftUI
 
-struct MasterView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+// MasterView receives View Model data structure from Content View. Masterview displays a list of NavigationLinks which correspond to each index within the things array. It then displays GUI and passes data for each indexed Thing instance to the DetailView.
 
-struct MasterView_Previews: PreviewProvider {
-    static var previews: some View {
-        MasterView()
+struct MasterView: View {
+    // View Model stores all information to be displayed in the View
+    @ObservedObject var viewModel: ViewModel
+    var body: some View {
+        List{
+            ForEach(viewModel.things) { thing in
+                NavigationLink(destination: DetailView(thing: thing, dvm: self.viewModel.detailViewModel[0])) {
+                    // Row View Stores functionality and UI instructions for each individual row
+                    RowView(thing: thing)
+                }
+                // On deletion of Instance, call function which removes Instance of wave from Wave array in View Model
+            }.onDelete { indices in
+                self.viewModel.removeThing(indices)
+            }
+        }
     }
 }
