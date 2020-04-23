89e5cc338329db18014e12b64d258dec14026221
Zak Barker <zakobama@hotmail.com>
2020-04-23 14:22:39 +1000

    Updated ContentView with functionality - not yet integrated

--- FavouriteThings/ContentView.swift
+++ FavouriteThings/ContentView.swift
@@ -8,14 +8,25 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
+// ContentView receives View Model data structure from Scene Delegate.swift. It then creates a NavigationView which houses the MasterView and passes the View Model structure to the MasterView for processing. Also provides an Add New Thing Function to update Model with a UI event.
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    // View Model stores all information to be displayed in the View
+    @ObservedObject var viewModel: ViewModel
+    var body: some View {
+        NavigationView{
+            MasterView(viewModel: viewModel)
+                .navigationBarTitle("World Breaks")
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing: Button(
+                        action: {
+                            withAnimation { self.viewModel.addThing() }
+                        }
+                    ) {
+                        Image(systemName: "plus")
+                    }
+                )
+        }
     }
 }
