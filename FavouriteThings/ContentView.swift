82719173109ed95e05f428049c3fb001cab6b26a
Zak Barker <zakobama@hotmail.com>
2020-04-23 13:43:34 +1000

    Initial Commit

---
+++ FavouriteThings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteThings
+//
+//  Created by Zak Barker on 23/4/20.
+//  Copyright © 2020 Zak Barker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
