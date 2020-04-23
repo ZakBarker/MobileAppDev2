4a0597f45a894a9995dd59506da60bb82af7cb85
Zak Barker <zakobama@hotmail.com>
2020-04-23 14:18:57 +1000

    Created ViewModel Class along with documentation

--- FavouriteThings/ViewModel.swift
+++ FavouriteThings/ViewModel.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+/// Class contains an array which stores data objects of the type Wave. This will be used to provide details to each view.
+class ViewModel: ObservableObject, Identifiable{
+    /// Stores data objects of type Wave
+    @Published var things: [Thing] = []
+    @Published var detailViewModel: [DetailViewModel] = []
+    
+    /// - Parameters:
+    ///     - things: Stores data objects of type Thing
+    ///     - detailViewModel: Stores data objects of type DetailViewModel
+    
+    /// Function appends new wave to waves array
+    func addThing(){
+        self.things.append(Thing(name: "New", like: "Wave", type: "", purpose: "", description: "", staticImage: "snapper"))
+    }
+    
+    /// Function removes wave from waves array
+    func removeThing(_ indices: IndexSet){
+        indices.forEach { thing.remove(at: $0) }
+    }
+    
+}
